63c2f0b40afcfc577c66362754db5f948563ffb0
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2022-01-11 21:31:09 +0530

    Create Mytemplate.cpp

---
+++ Mytemplate.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// function to check a no. is palindrome or not
+int ispalindrome(string s){
+    int len= s.length();
+    
+    for(int i=0 ;i<len/2 ;i++){
+        if(s[i]!= s[len-i-1])return false;
+    }
+    
+int main(){
+
+
+
+return 0;
+}
