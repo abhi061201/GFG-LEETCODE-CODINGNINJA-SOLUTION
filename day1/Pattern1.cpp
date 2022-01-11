42f289b3d6b450272f80862fae684da65fe3f9b3
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2022-01-11 18:31:15 +0530

    Update Pattern1.cpp

--- day1/Pattern1.cpp
+++ day1/Pattern1.cpp
@@ -1,2 +1,20 @@
-# MyPractice
-dailypractice
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int n;
+	cin>>n;
+	
+	for(int i=1; i<=n ;i++){
+	    int k=i;
+	    while(k++<n) cout<<" ";
+	  for(int j=n-i; j<n ;j++){
+	      cout<<"*";
+	  }
+	  cout<<"\n";
+	    
+	    
+	}
+	return 0;
+}
