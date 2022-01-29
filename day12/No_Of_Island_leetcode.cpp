00fcbd4bf14afb0695fb1fb446f7b95c3a29aa6c
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2022-01-29 11:32:23 +0530

    Update No_Of_Island_leetcode.cpp

--- day12/No_Of_Island_leetcode.cpp
+++ day12/No_Of_Island_leetcode.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
    
-    void dfs(int x,int y,vector<vector<char>>& grid,vector<vector<int>>&vis,int m,int n){
+    void dfs(int x,int y,vector<vector<char>>& grid,vector<vector<int>>&vis,int m,int n,char c){
         vis[x][y]=1;
+        grid[x][y]=c;
         if(x+1<m && x+1>=0 && y<n && y>=0){
             if(!vis[x+1][y] && grid [x+1][y]=='1'){
                 vis[x+1][y]=1;
-                dfs(x+1,y,grid,vis,m,n);
+                grid [x+1][y]=c;
+                dfs(x+1,y,grid,vis,m,n,c);
             }
         }
         if(x-1<m && x-1>=0 && y<n && y>=0){
             if(!vis[x-1][y] && grid[x-1][y]=='1'){
                 vis[x-1][y]=1;
-                dfs(x-1,y,grid,vis,m,n);
+                grid[x-1][y]=c;
+                dfs(x-1,y,grid,vis,m,n,c);
             }
         }
         if(x<m && x>=0 && y+1<n && y+1>=0){
             if(!vis[x][y+1] && grid[x][y+1]=='1'){
                 vis[x][y+1]=1;
-                dfs(x,y+1,grid,vis,m,n);
+                grid[x][y+1]=c;
+                dfs(x,y+1,grid,vis,m,n,c);
             }
         }
         if(x<m && x>=0 && y-1<n && y-1>=0){
             if(!vis[x][y-1] && grid [x][y-1]=='1'){
                 vis[x][y-1]=1;
-                dfs(x,y-1,grid,vis,m,n);
+                grid [x][y-1]=c;
+                dfs(x,y-1,grid,vis,m,n,c);
             }
         }
         
@@ -36,18 +41,28 @@ public:
         int n= grid[0].size();
         vector <vector <int>>vis(m, vector<int>(n,0));
         int count=0;
+        char c='0';
         for(int i=0 ;i<m ;i++){
             for(int j=0 ;j<n ;j++){
                 if(grid[i][j]=='1' && !vis[i][j]){
                     count++;
-               
-                    dfs(i,j,grid,vis,m,n);
+                    c++;
+                    
+                    
+                    dfs(i,j,grid,vis,m,n,c);
                 }
             }
         }  
+         for(int i=0 ;i<m ;i++){
+            for(int j=0 ;j<n ;j++){
+                cout<<grid[i][j]<<" , ";
+                
+                }
+             cout<<"\n";
+            }
+        
         
         return count;
         
-        
-    }
-};
+}
+    };
