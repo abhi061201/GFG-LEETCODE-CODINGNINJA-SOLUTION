d7db615140d8d398aadc2e9b7d7a6a5699b0ccaa
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2022-02-01 04:36:32 +0530

    Create town Judge.cpp

---
+++ day14/town Judge.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        
+        vector <pair <int,int>> v(n+1, {0,0});
+        for(auto it : trust){
+            v[it[0]].first++;
+            v[it[1]].second++;
+            
+            
+        }
+        for(int i=1 ;i<=n ;i++){
+            if(v[i].first==0 && v[i].second==n-1)return i;
+        }
+        return -1;
+        
+        
+    }
+};
