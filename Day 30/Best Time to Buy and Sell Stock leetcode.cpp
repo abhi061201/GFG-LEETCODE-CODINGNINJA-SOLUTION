49c6c7ad4e8d9dffc4e0873abc069d9a57d9f4d2
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2022-02-19 11:28:20 +0530

    Create Best Time to Buy and Sell Stock leetcode.cpp

---
+++ Day 30/Best Time to Buy and Sell Stock leetcode.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& p) {
+        
+        int n= p.size();
+        int m_ele=INT_MAX;
+        int pro=0;
+        for(int i=0 ;i<n ;i++){
+            m_ele=min(m_ele,p[i]);
+            pro= max(pro,p[i]-m_ele);
+        }
+        return pro;
+    }
+};
