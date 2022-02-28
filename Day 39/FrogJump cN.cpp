ecaf074b7a147ebbbf5867cd76ccd9c069dc1ec2
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2022-02-28 16:59:21 +0530

    Update FrogJump cN.cpp

--- Day 39/FrogJump cN.cpp
+++ Day 39/FrogJump cN.cpp
@@ -1,3 +1,6 @@
+TC= O(N)
+SC= O(N)+O(N)
+
 #include<bits/stdc++.h>
 int go(int i,vector<int>&arr,vector<int>&dp){
     if(i==0)return 0;
@@ -14,4 +17,35 @@ int frogJump(int n, vector<int> &arr)
 {
     vector<int>dp(n,-1);
    return go(n-1,arr,dp);
+}
+
+
+
+
+// Space optimised
+TC= O(N)
+SC= constant
+
+
+#include<bits/stdc++.h>
+int frogJump(int n, vector<int> &arr)
+{
+    int p2= 0;
+    int p1= 0;
+    for(int i=1 ;i<n ;i++){
+        int left= p1+abs(arr[i]-arr[i-1]);
+        int ss= INT_MAX;
+        if(i>1){
+            ss= p2+abs(arr[i]-arr[i-2]);
+        }
+        int val= min(left,ss);
+//         cout<<val<<" , "<<p1<<" , "<<p2<<" , \n";
+        p2=p1;
+        p1=val;
+      
+        
+    }
+    return p1;
+    
+    
 }
