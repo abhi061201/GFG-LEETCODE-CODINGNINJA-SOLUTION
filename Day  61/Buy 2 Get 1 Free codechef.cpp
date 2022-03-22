4294b96c314cd62d13b3899c21507e8c2f6d3177
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2022-03-22 11:40:59 +0530

    Create Buy 2 Get 1 Free codechef.cpp

---
+++ Day  61/Buy 2 Get 1 Free codechef.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+// function to count unique elements from a vector
+int uniqueele(vector<ll> v)
+{
+
+    int n = v.size();
+    unordered_map<ll, ll> mp;
+    for (auto x : v)
+    { // auto keyword is used for automatic typename
+        mp[x]++;
+    }
+    //if we want to print elements also 
+    //         for(auto y :mp){
+    //             cout<<y.first <<" "<< y.second<<"\n";
+    //         }
+    return mp.size();
+}
+
+// function to check a no. is palindrome or not
+int ispalindrome(string s){
+    int len= s.length();
+    
+    for(int i=0 ;i<len/2 ;i++){
+        if(s[i]!= s[len-i-1])return false;
+    }}
+    
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);
+ll t;
+    cin>>t;
+while(t-->0){
+    
+   ll n, x;
+   cin>>n>>x;
+   ll ans= (n/3)*2*x;
+   ans+=(n%3)*x;
+   cout<<ans<<endl;
+    
+}
+return 0;
+}
