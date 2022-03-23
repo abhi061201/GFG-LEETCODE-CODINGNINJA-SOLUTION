f12343c6ad171ebb7847d1100ef607dc8bd437a2
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2022-03-23 17:51:03 +0530

    Update Subset Sum Equal To K CN .cpp

--- Day   62/Subset Sum Equal To K CN .cpp
+++ Day   62/Subset Sum Equal To K CN .cpp
@@ -1,3 +1,7 @@
+// Top down 
+TC= O(N*target)
+TC= O(N* target) + O(N)    
+
 bool go(vector<int> &arr, int i, int t,vector<vector<int>>&dp){
     if(t==0)return true;
     if(i==0)return t==arr[0];
@@ -11,6 +15,22 @@ bool go(vector<int> &arr, int i, int t,vector<vector<int>>&dp){
 
 bool subsetSumToK(int n, int k, vector<int> &arr) {
     // Write your code here.
-    vector<vector<int>>dp(n+1, vector<int>(k+1,-1));
-    return go(arr, n-1, k,dp);
+    vector<vector<int>>dp(n+1, vector<int>(k+1, 0));
+    dp[0][0]=1;
+    for(int i=1 ; i<=n ;i++)dp[i][0]=1;
+    
+    for(int i=1 ; i<=n ; i++){
+        for(int target=1 ; target<=k ; target++){
+            dp[i][target]= dp[i-1][target] ;
+            if(target>=arr[i-1]){
+                dp[i][target]= dp[i-1][target]|| dp[i-1][target-arr[i-1]];
+            }
+        }
+    }
+    return dp[n][k];
+    
 }
+
+// bottom up 
+// TC= O(N*Target)
+// Sc = O(N*target)
