a9c707bee7609e939b4c47c5242251eb7d82aef5
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2022-03-26 12:05:26 +0530

    Create 53. Maximum Subarray leetcode.cpp

---
+++ Day   65/53. Maximum Subarray leetcode.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int n= nums.size();
+        int sum=0;
+        int maxi=nums[0];
+        
+        for(int i=0 ; i<n ; i++){
+            sum+=nums[i];
+            maxi= max(sum, maxi);
+            if(sum<0)sum=0;
+            
+        }
+        return maxi;
+    }
+};
