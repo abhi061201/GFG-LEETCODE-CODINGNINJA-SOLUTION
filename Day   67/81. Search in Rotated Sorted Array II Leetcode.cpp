791f4d5d1e7fb3146257ff7b030b0578fad5cb15
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2022-03-28 09:21:23 +0530

    Update 81. Search in Rotated Sorted Array II Leetcode.cpp

--- Day   67/81. Search in Rotated Sorted Array II Leetcode.cpp
+++ Day   67/81. Search in Rotated Sorted Array II Leetcode.cpp
@@ -6,3 +6,19 @@ public:
         return false;
     }
 };
+
+// nlogn
+
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int t) {
+       
+        int i=0, j= nums.size()-1;
+        while(i<=j){
+            if(nums[i]==t  || nums[j]==t)return true;
+            i++;j--;
+        }
+    return false;
+    }
+};
