1a5ef089be8f5f02524b1d4914adeb2972634081
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2022-04-01 07:38:14 +0530

    Update 1523. Count Odd Numbers in an Interval Range leetcode.cpp

--- Day    71/1523. Count Odd Numbers in an Interval Range leetcode.cpp
+++ Day    71/1523. Count Odd Numbers in an Interval Range leetcode.cpp
@@ -9,3 +9,15 @@ public:
         return odd;
     }
 };
+
+
+
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        
+      int odd=(high-low)/2;
+        if(high%2!=0 || low%2!=0)odd++;        
+        return odd;
+    }
+};
