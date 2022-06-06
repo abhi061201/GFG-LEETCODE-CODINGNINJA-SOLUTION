3a26c48bdb24232250e0ff73bba4eb19525acfec
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2022-06-06 09:39:31 +0530

    Create intersection linkedlist leetcode.cpp

---
+++ Day         127/intersection linkedlist leetcode.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *h1, ListNode *h2) {
+        
+        ListNode* t1=h1;
+        ListNode* t2=h2;
+        if(!t1|| !t2)return NULL;
+        while(t1!=t2){
+            
+            if(t1==t2)return t1;
+            
+            if(t1)t1=t1->next;
+            else t1=h2;
+            if(t2)t2=t2->next;
+            else t2=h1;
+            
+        }
+        return t1;
+       
+    }
+    
+};
