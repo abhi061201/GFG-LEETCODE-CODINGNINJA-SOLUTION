d9b90d73e1b311220bf14098ad67b486e1bc8d27
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2022-06-13 10:10:44 +0530

    Create traingle .cpp

---
+++ Day          132/traingle .cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& tri) {
+        int n= tri.size();
+        
+        vector<vector<int>>dp(n,vector<int>(n,1e9));
+        for(int i=0 ; i<n ; i++){
+            dp[n-1][i]=tri[n-1][i];
+            
+        }
+        for(int i=n-2 ; i>=0 ; i--){
+            for(int j=0; j<=i; j++){
+                dp[i][j]=min(dp[i+1][j],dp[i+1][j+1])+ tri[i][j];
+            }
+        }
+        return dp[0][0];
+    }
+};
