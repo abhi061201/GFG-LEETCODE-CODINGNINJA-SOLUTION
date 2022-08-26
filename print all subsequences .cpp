c440d8738920995653e0cc29543c6f7fad556c54
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2022-08-26 12:41:58 +0530

    Update print all subsequences .cpp

--- print all subsequences .cpp
+++ print all subsequences .cpp
@@ -1,3 +1,5 @@
+POWER SET
+=========
 TC-- O(n*(2^n));
 sc= O(1)
 
@@ -31,5 +33,46 @@ int main(){
 }
 
 
+
+==========================================================
+    
+    RECURSIVE APPROACH
+    ======
+    
+ TC=O(2^n)
+sc= O(1)
+
+#include<iostream>
+using namespace std;
+
+class print{
+    public:
+    void printallsubseq(string s){
+        go(0, s,"");
+    }
+    void go(int i, string s, string temp){
+        if(i==s.length()){
+            cout<<temp<<endl;
+            return ;
+        }
+        temp.push_back(s[i]);
+        go(i+1, s,temp);
+        temp.pop_back();
+        go(i+1, s, temp);
+    }
+    
+    
+};
+int main(){
+    
+    string s;
+    cin>>s;
+    print p;
+    p.printallsubseq(s);
+    
+    
+    
+    return 0;
+}
 
     
