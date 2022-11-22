a473ebcdbda2922fe62a026a99011751af6ae03b
Abhishek Gupta <ag742757@gmail.com>
2022-11-22 12:40:51 +0530

    Time: 24 ms (95.00%), Space: 9.1 MB (51.59%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int numSquares(int n) {
-        return go(n);
+        vector<int>dp(n+1,-1);
+        return go(n,dp);
     }
-    int go(int n)
+    int go(int n,vector<int>&dp)
     {
         if(n<=3)return n;
+        if(dp[n]!=-1)return dp[n];
         int t= sqrt(n);
         int mini= 1e9;
         for(int i=t; i>1; i--)
         {
-            mini = min(mini, go(n%(i*i)) + n/(i*i));
+            mini = min(mini, go(n%(i*i),dp) + n/(i*i));
         }
-        return mini;
+        return dp[n]=mini;
     }
 };
