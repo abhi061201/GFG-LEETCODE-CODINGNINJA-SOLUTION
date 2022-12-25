b08c7969ec06fc5121f58c31739a8c23a09817c2
Abhishek Gupta <ag742757@gmail.com>
2022-12-25 11:11:43 +0530

    Time: 3 ms (34.98%), Space: 6.2 MB (29.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return go(n, dp);
+        return go(n,dp);
     }
-    int go(int n, vector<int>&dp)
+    int go(int i, vector<int>&dp)
     {
-        if(n<0)return 0;
-        if(n==0)return 1;
-        if(dp[n]!=-1)return dp[n];
-        int one=0, two=0;
-        one = go(n-1,dp);
-        two= go(n-2,dp);
-        return dp[n]=one+two;
+        if(i<0)return 0;
+        if(i==0)return 1;
+        if(dp[i]!=-1)return dp[i];
+        int one= go(i-1,dp);
+        int two= go(i-2,dp);
+        return dp[i]=one+two;
     }
 };
