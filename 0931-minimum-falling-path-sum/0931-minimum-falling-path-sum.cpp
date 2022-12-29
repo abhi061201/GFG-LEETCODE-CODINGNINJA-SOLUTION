4055d83fac9ab25567e7df89f69aea0369e43300
Abhishek Gupta <ag742757@gmail.com>
2022-12-29 20:01:25 +0530

    Time: 17 ms (76.12%), Space: 10.3 MB (20.06%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     int n;
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-         n= matrix.size();
-        int ans= 1e9;
-        vector<vector<int>>dp(n, vector<int>(n,-1));
+    int minFallingPathSum(vector<vector<int>>& mat) { 
+        int ans=1e9;
+        n= mat.size();
+        vector<vector<int>>dp(n, vector<int>(n, -1));
         for(int i=0; i<n; i++)
         {
-            ans= min(ans, go(0,i, matrix,dp));
+            ans= min(ans, go(0,i,mat, dp));      
         }
-        return ans;
+        return ans;      
     }
-    
-    int go(int i, int j, vector<vector<int>>&matrix,vector<vector<int>>&dp)
+    int go(int i, int j, vector<vector<int>>&mat,vector<vector<int>>&dp)
     {
-        if(i==n){
-            return 0;
-        }
+        if(i==mat.size())return 0;
         if(dp[i][j]!=-1)return dp[i][j];
-        int l=1e9, r=1e9, d=1e9;
-        
-        if(j-1>=0 && j-1 <n)l= matrix[i][j]+ go(i+1,j-1,matrix, dp);
-        if(j>=0 && j <n)d= matrix[i][j]+ go(i+1,j,matrix, dp);
-        if(j+1>=0 && j+1 <n)r= matrix[i][j]+ go(i+1,j+1,matrix, dp );
-        return dp[i][j] = min({l,d,r});
-        
+        int left=1e9, down=1e9, right=1e9;
+        if(j-1 >=0 )left= go(i+1, j-1, mat,dp);
+        down= go(i+1, j, mat,dp);
+        if(j+1 < n)right= go(i+1, j+1, mat,dp);
+        return dp[i][j]=mat[i][j]+ min({left, down, right});
     }
 };
