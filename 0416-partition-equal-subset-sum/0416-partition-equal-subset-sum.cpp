d9311264f2a47023ed42290857ec5d724bada181
Abhishek Gupta <ag742757@gmail.com>
2023-01-06 14:33:32 +0530

    Time: 714 ms (19.20%), Space: 95.6 MB (16.75%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -4,8 +4,22 @@ public:
         int sum=accumulate(nums.begin(), nums.end(),0), n= nums.size();
         
         if(sum&1)return 0;
-        vector<vector<int>>dp(n, vector<int>(sum/2 +1, -1));
-        return go(n-1, nums, sum/2, dp);
+        vector<vector<int>>dp(n+1, vector<int>(sum/2 +1, 0));
+        for(int k=1; k<=sum/2; k++)dp[0][k]=0;
+        dp[0][0]=1;
+        for(int i=1; i<=n; i++)
+        {
+            dp[i][0]=1;
+            for(int k=0 ; k<=sum/2 ; k++)
+            {
+                int take=0, n_take=0;
+                if(nums[i-1]<= k)take= dp[i-1][k-nums[i-1]];
+                n_take= dp[i-1][k];
+                dp[i][k]=take|n_take;   
+            }
+        }
+        
+        return dp[n][sum/2];
     }
     int go(int i, vector<int>&nums, int k,vector<vector<int>>&dp)
     {
