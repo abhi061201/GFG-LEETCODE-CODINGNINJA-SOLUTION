1d41fd17e4409f3e383d5569d2328650b956e174
Abhishek Gupta <ag742757@gmail.com>
2023-01-06 16:25:19 +0530

    Time: 2099 ms (10.28%), Space: 9.5 MB (69.89%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,25 +1,28 @@
 class Solution {
-    public:
-    bool helper(int i, int k, int cursum, int target, vector<bool>&v, vector<int>&nums){
-        if(i==nums.size()) return false;
-        if(k==1) return true;
-        if(cursum>target) return false;
-        if(cursum==target){
-            return helper(0,k-1,0,target,v,nums);
-        }
-        for(int j=i;j<nums.size();j++){
-            if(v[j]==true) continue;
-            v[j]=true;
-            if(helper(j+1,k,cursum+nums[j],target,v,nums)) return true;
-            v[j]=false;
-        }
-        return false;
-    }
+public:
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        int sum=accumulate(nums.begin(),nums.end(),0);
-        int target=sum/k,n=nums.size();
-        vector<bool>v(n,false);
-        if(k>n||sum%k!=0) return false;
-        return helper(0,k,0,target,v,nums);
+        int sum = accumulate(nums.begin(), nums.end(), 0), n= nums.size();
+        if(n<k || sum%k)return 0;
+        vector<int>vis(n,0);
+        sort(nums.begin() , nums.end(), greater<int>());
+        return go(0,0, sum/k,k, nums , vis );
+            
+    }
+    bool go(int i, int cursum, int t_sum, int k, vector<int>& nums,vector<int>&vis )
+    {
+        if(i==nums.size())return 0;
+        if(k==1)return 1;
+        if(cursum>t_sum)return 0;
+        if(cursum==t_sum)return go(0,0,t_sum, k-1, nums,vis);
+        bool ans=false;
+        for(int start=i; start<nums.size(); start++ )
+        {
+            if(vis[start]==1)continue;
+            vis[start]=1;
+            ans = ans|| go(start+1, cursum+nums[start], t_sum, k, nums, vis);
+            
+            vis[start]=0;
+        }
+        return ans;
     }
 };
