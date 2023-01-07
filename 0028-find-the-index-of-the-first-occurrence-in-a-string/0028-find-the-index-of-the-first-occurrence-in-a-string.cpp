61ff7fe78193e09dfcab94f158dd4b78bc1c613e
Abhishek Gupta <ag742757@gmail.com>
2023-01-08 01:25:03 +0530

    Time: 4 ms (37.22%), Space: 6.6 MB (11.69%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int strStr(string hs, string ne) {
-        int n= hs.length();
-        int m= ne.length();
-        string sub;
-        for(int i=0; i<=n-m; i++)
-        {   
-             if(ne[0]==hs[i])
-             {
-                 sub= hs.substr(i,m);
-                 if(sub==ne)return i;
-             }
+        int n= hs.length(), m= ne.length();
+        vector<int>lps(n+m+1,0);
+        string temp= ne+"&"+hs;
+        for(int i=1; i<n+m+1; i++)
+        {
+            if(temp[i]==temp[lps[i-1]]){
+                lps[i]= 1+ lps[i-1];
+            }
+            else {
+                int j= lps[i-1];
+                while(j>0 && temp[i]!= temp[j])
+                {
+                    j= lps[j-1];
+                }
+                if(temp[i]==temp[j])j++;
+                lps[i]= j;
+            }
         }
         
+        for(int i=0; i<n+m+1; i++)if(lps[i]==m)return i-2*m;
         return -1;
+        
     }
 };
