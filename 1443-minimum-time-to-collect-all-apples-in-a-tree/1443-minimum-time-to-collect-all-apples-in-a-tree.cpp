7a6654dc1ce0c264a3e64800431c7ba68c885b3c
Abhishek Gupta <ag742757@gmail.com>
2023-01-12 20:23:13 +0530

    Time: 201 ms (76.16%), Space: 64.9 MB (40.73%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -3,27 +3,22 @@ public:
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& app) 
     {
         vector<vector<int>>adj(n);
-         vector<vector<int>>tempedges={{0,2},{0,3},{1,2}};
-        vector<bool>tempapp= {false,true,false,false};
-        
-        if(edges==tempedges && app== tempapp)return 4;
         for(auto it: edges)
         {
             adj[it[0]].push_back(it[1]);
-            
-            
+            adj[it[1]].push_back(it[0]);
         }
         
-      int ans=dfs(0,adj,app);
+      int ans=dfs(0,adj,app,-1);
         cout<<ans;
         return ans>0?ans-2:0;
     }
-    int dfs(int i,vector<vector<int>>&adj, vector<bool>& app )
+    int dfs(int i,vector<vector<int>>&adj, vector<bool>& app, int par )
     {
         int ans=0;
         for(auto it: adj[i])
         {
-            ans+= dfs(it,adj,app);
+            if(it!=par)ans+= dfs(it,adj,app,i);
         }
         if(ans>0)return 2+ans;
         if(app[i])return 2;
