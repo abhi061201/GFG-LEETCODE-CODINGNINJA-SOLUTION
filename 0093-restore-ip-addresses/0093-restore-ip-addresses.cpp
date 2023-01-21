ed6e5a8599b438a800ae898681007f03e7dc483e
Abhishek Gupta <ag742757@gmail.com>
2023-01-22 01:23:02 +0530

    Time: 5 ms (41.81%), Space: 6.4 MB (97.91%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    // Helper function to check if a given string is a valid IP address segment
+    bool valid(string temp){
+        if(temp.size()>3 || temp.size()==0) return false; // segment length should be between 1 and 3
+        if(temp.size()>1 && temp[0]=='0')   return false; // segment should not start with 0, unless it is a single digit
+        if(temp.size() && stoi(temp)>255) return false; // segment should not be greater than 255
+        return true;
+    }
+
+    // Backtracking function to generate all possible IP addresses
+    void solve(vector<string>& ans, string output, int ind, string s, int dots){
+        if(dots == 3){ // if we have already added 3 dots, check if the remaining segment is valid
+            if(valid(s.substr(ind)))
+                ans.push_back(output+s.substr(ind));
+            return;
+        }
+        int sz = s.size();
+        for(int i=ind;i<min(ind+3, sz);i++){
+            if(valid(s.substr(ind, i-ind+1))){
+                output.push_back(s[i]);
+                output.push_back('.');
+                solve(ans, output, i+1, s, dots+1);
+                output.pop_back(); //backtrack
+            }
+        }
+
+    }
+
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> ans;
+        string res;
+        solve(ans, res, 0, s, 0);
+        return ans;
+    }
+};
