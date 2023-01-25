cb3550c759fc57f7acfccba44730bfb5e7841e78
Abhishek Gupta <ag742757@gmail.com>
2023-01-26 00:55:59 +0530

    Time: 122 ms (76.05%), Space: 75.7 MB (36.30%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -3,30 +3,32 @@ public:
     vector<vector<string>>ans;
     vector<vector<string>> partition(string s) {
         vector<string>temp;
-        go(0, s, temp);
+        go(0,s,temp);
         return ans;
     }
-    void go(int i, string &s, vector<string>&temp)
+    void go(int i, string & s, vector<string>&temp)
     {
         if(i==s.length())
         {
             ans.push_back(temp);
+            return ;
         }
         string str;
-        for(int start= i; start<s.length(); start++)
+        for(int j=i; j<s.length();j++)
         {
-            str.push_back(s[start]);
-            if(ispali(str))
+            str.push_back(s[j]);
+            if(ispalli(str))
             {
                 temp.push_back(str);
-                go(start+1, s, temp);
+                go(j+1, s, temp);
                 temp.pop_back();
             }
+       
         }
     }
-    bool ispali(string &s)
+    bool ispalli(string &s)
     {
-        int i=0, j = s.length()-1;
+        int i=0, j= s.length()-1;
         while(i<j)
         {
             if(s[i++]!= s[j--])return 0;
