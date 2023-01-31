b16b1894a9e9447e73b31eb2f0146f85e6112d45
Abhishek Gupta <ag742757@gmail.com>
2023-01-31 23:20:01 +0530

    Time: 22 ms (76.62%), Space: 31.4 MB (5.43%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -1,31 +1,67 @@
-class Solution {
-public:
-    vector<string> twoEditWords(vector<string>& queries, vector<string>& dic) {
-        
-        vector<string>ans;
-        for(int i=0; i<queries.size(); i++)
+class TrieNode
+{
+    public:
+    TrieNode* links[26];
+    int eow;
+    TrieNode()
+    {
+        eow=0;
+        for(int i=0; i<26;i++)links[i]=NULL;
+    }
+};
+class Trie
+{
+    public:
+    TrieNode* root;
+    Trie()
+    {
+        root= new TrieNode();
+    }
+    
+    void insert(string &s)
+    {
+        TrieNode* temp= root;
+        for(auto it: s)
         {
-            if(check(queries[i], dic))ans.push_back(queries[i]);
+            if(!temp->links[it-'a'])temp->links[it-'a']= new TrieNode();
+            temp= temp->links[it-'a'];
+        }
+        temp->eow++;
+    }
+    bool checkmatch(string &s)
+    {
+        TrieNode*temp = root;
+        
+        return go(temp, s, 2, 0);
+    }
+    bool go(TrieNode* temp, string & s, int steps, int ind)
+    {
+        if(steps<0 || ind==s.length())return steps>=0;
+        bool ans=0;
+        for(int i=0; i<26; i++)
+        {
+            if(temp->links[i])
+            {
+                ans= ans|go(temp->links[i], s, steps- (i+'a'!=s[ind]), ind+1 );
+            }
         }
         return ans;
-        
     }
-    bool check(string &s, vector<string>& dic)
-    {
-        
-       for(auto it: dic)
-       {
-           if(it.length()==s.length())
-           {
-                int temp=0;   
-               for(int i=0; i<s.length() ; i++)
-               {
-                   if(s[i]!= it[i])temp++;
-                   if(temp>2)break;
-               }
-               if(temp<=2)return 1;
-           }
-       }
-        return 0;
+    
+};
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        Trie t;
+        for(auto it: dictionary)
+        {
+            t.insert(it);
+        }
+        vector<string >ans;
+        for(auto it: queries)
+        {
+            if(t.checkmatch(it))ans.push_back(it);
+        }
+        return ans;
     }
 };
