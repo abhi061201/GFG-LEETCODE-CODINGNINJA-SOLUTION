2cb02d209fa4c4125c9288bf11dc9fe7462a09a8
Abhishek Gupta <ag742757@gmail.com>
2023-02-04 09:56:10 +0530

    Time: 289 ms (12.44%), Space: 121.7 MB (5.01%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        s1= sortString(s1);
-        // sort(s1.begin(), s1.end());
+        // 
+        vector<int>v1(26,0),v2;
+        v1= sortString(s1);
+        
         string temp;
         int n= s2.length(), m= s1.length();
         unordered_set<char>se(s2.begin(), s2.end());
@@ -11,29 +13,22 @@ public:
             if(se.find(s2[i])!=se.end())
             {
                 temp= s2.substr(i,m);
-                temp= sortString(temp);
-                // sort(temp.begin(), temp.end());
-                if(temp==s1)return 1;
+                
+                v2= sortString(temp);
+                
+                if(v2==v1)return 1;
             }
         }
         return 0;
     }
-    string sortString(string s)
+    vector<int> sortString(string s)
     {
         vector<int>v(26,0);
         for(auto it: s)
         {
             v[it-'a']++;
         }
-        s="";
-        for(int i=0; i<26; i++)
-        {
-            int k= v[i];
-            while(k--)
-            {
-                s.push_back(i+'a');
-            }
-        }
-        return s;
+       
+        return v;
     }
 };
