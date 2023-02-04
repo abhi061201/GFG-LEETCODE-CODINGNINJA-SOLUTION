4c1b7d5096e31883457774d6eb662a4cf2b4f93f
Abhishek Gupta <ag742757@gmail.com>
2023-02-04 09:50:03 +0530

    Time: 436 ms (10.83%), Space: 121.6 MB (5.01%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        // s1= sortString(s1);
-        sort(s1.begin(), s1.end());
+        s1= sortString(s1);
+        // sort(s1.begin(), s1.end());
         string temp;
         int n= s2.length(), m= s1.length();
         unordered_set<char>se(s2.begin(), s2.end());
@@ -11,27 +11,29 @@ public:
             if(se.find(s2[i])!=se.end())
             {
                 temp= s2.substr(i,m);
-                // temp= sortString(temp);
-                sort(temp.begin(), temp.end());
+                temp= sortString(temp);
+                // sort(temp.begin(), temp.end());
                 if(temp==s1)return 1;
             }
         }
         return 0;
     }
-//     string sortString(string s)
-//     {
-//         vector<int>v(26,0);
-//         for(auto it: s)
-//         {
-//             v[it-'a']++;
-//         }
-//         s="";
-//         for(int i=0; i<26; i++)
-//         {
-//             if(v[i]){
-                
-//             }
-//         }
-//         return "";
-//     }
+    string sortString(string s)
+    {
+        vector<int>v(26,0);
+        for(auto it: s)
+        {
+            v[it-'a']++;
+        }
+        s="";
+        for(int i=0; i<26; i++)
+        {
+            int k= v[i];
+            while(k--)
+            {
+                s.push_back(i+'a');
+            }
+        }
+        return s;
+    }
 };
