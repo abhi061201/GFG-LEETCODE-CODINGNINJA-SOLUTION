3edef33bf890cf649e6ea225b90eead71a973c8b
Abhishek Gupta <ag742757@gmail.com>
2023-02-04 09:48:41 +0530

    Time: 2130 ms (5.02%), Space: 59.6 MB (5.53%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        // s1= sortString(s1);
+        sort(s1.begin(), s1.end());
+        string temp;
+        int n= s2.length(), m= s1.length();
+        unordered_set<char>se(s2.begin(), s2.end());
+        for(int i=0; i<=n-m; i++)
+        {
+            if(se.find(s2[i])!=se.end())
+            {
+                temp= s2.substr(i,m);
+                // temp= sortString(temp);
+                sort(temp.begin(), temp.end());
+                if(temp==s1)return 1;
+            }
+        }
+        return 0;
+    }
+//     string sortString(string s)
+//     {
+//         vector<int>v(26,0);
+//         for(auto it: s)
+//         {
+//             v[it-'a']++;
+//         }
+//         s="";
+//         for(int i=0; i<26; i++)
+//         {
+//             if(v[i]){
+                
+//             }
+//         }
+//         return "";
+//     }
+};
