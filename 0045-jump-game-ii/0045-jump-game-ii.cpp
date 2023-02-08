0d16f706ce58256dbba920d0e1967d322ac02db2
Abhishek Gupta <ag742757@gmail.com>
2023-02-08 10:02:08 +0530

    Time: 579 ms (16.05%), Space: 18.2 MB (6.85%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     int n;
     int jump(vector<int>& nums) {
-        n= nums.size();
+      n= nums.size();
         vector<int>dp(n,-1);
-        return go(0, nums, dp);
+        return go(0,nums,dp);
     }
-    int go(int i, vector<int>& nums,vector<int>&dp)
+    int go(int i, vector<int>&nums,vector<int>&dp)
     {
-        if(i>=n-1)return 0;
-        int ans=n;
+        if(i==nums.size()-1)return 0;
         if(dp[i]!=-1)return dp[i];
-        for(int j=1; j<=nums[i]; j++)
+        int ans=1e9;
+        for(int j=1; j<=nums[i]&& i+j <n ; j++ )
         {
-            
-            if(i+j<n)ans= min(ans, 1+go(i+j, nums, dp));
+            ans= min(ans, 1+go(i+j, nums,dp));
         }
-        return dp[i]= ans;
+        return dp[i]=ans;
     }
 };
