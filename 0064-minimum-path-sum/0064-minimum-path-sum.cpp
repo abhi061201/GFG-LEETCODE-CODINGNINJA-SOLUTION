d7d343d3c5bd61aecb8af53e5be5f4e57fe83475
Abhishek Gupta <ag742757@gmail.com>
2023-03-27 13:59:05 +0530

    Time: 10 ms (65.71%), Space: 10.2 MB (7.26%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
-    int n,m;
     int minPathSum(vector<vector<int>>& grid) {
-        n= grid.size();
-        m= grid[0].size();
-      
+        int n= grid.size(), m= grid[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return go(n-1,m-1,grid,dp);
         
-        for(int i=1; i<n;i++)grid[i][0] += grid[i-1][0];
-        for(int j=1; j<m; j++)grid[0][j]+= grid[0][j-1];
-        
-        for(int i=1;i<n;i++)
-        {
-            for(int j=1; j<m ;j++)
-            {
-                grid[i][j]+= min(grid[i-1][j], grid[i][j-1]);
-            }
-            
-        }
-        return grid[n-1][m-1];
-        
-       
     }
-    int go(int i, int j, vector<vector<int>>& grid,vector<vector<int>>&dp)
+    int go(int i, int j, vector<vector<int>>&grid,vector<vector<int>>&dp)
     {
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size())return 1e9;
         if(i==0 && j==0)return grid[i][j];
         if(dp[i][j]!=-1)return dp[i][j];
-        int left=1e9, up=1e9;
-        if(i-1>=0)left= go(i-1, j,grid,dp);
-        if(j-1>=0)up= go(i,j-1, grid,dp);
-        return dp[i][j]= grid[i][j]+ min(left,up);
+        int mini=0;
+        int up = go(i,j-1,grid,dp);
+        int left= go(i-1,j, grid,dp);
+        return dp[i][j]= grid[i][j]+ min({up,left});      
     }
 };
